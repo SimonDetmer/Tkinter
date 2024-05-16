4d3d6092c0065414116f5e13b40ca0a69d801594
simon <simon.detmer@googlemail.com>
2024-05-16 15:33:00 +0200

    Update-16-05-24-01

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,6 +2,13 @@ from tkinter import *
 
 root = Tk()
 
+header = Frame(root)
+header.pack()
+mainFrame = Frame(root)
+mainFrame.pack()
+footer = Frame(root)
+footer.pack(fill=X)
+
 text = '''
 Ich
 bin
@@ -15,14 +22,19 @@ def client_exit():
     root.destroy()
 
 
-w = Label(root, text=text, fg='blue', bg='yellow', justify=LEFT)
-w.pack()
 
-w2 = Label(root, text='Hi', fg='white', bg='red', font='courier')
+
+w3 = Label(root, text='foo', fg='blue', bg='yellow', justify=LEFT)
+w3.pack()
+
+w2 = Label(header, text='Hi', fg='white', bg='red', font='courier')
 w2.pack(side=LEFT)
 
-b = Button(root, text='Exit', command=client_exit)
-b.pack()
+w = Label(mainFrame, text=text, fg='blue', bg='yellow', justify=LEFT)
+w.pack()
+
+b = Button(footer, text='Exit', command=client_exit)
+b.pack(fill=X)
 
 if __name__ == '__main__':
     root.mainloop()
---
+++ ImageViewer.py
@@ -0,0 +1,64 @@
+import os
+import glob
+
+from tkinter import *
+from PIL import ImageTk, Image
+
+def resize(image, x):
+    w, h = image.size
+    return image.resize((x, int(h*x/w)), Image.BILINEAR)
+
+class ImageViewer:
+    def __init__(self, root):
+        self.root = root
+        self.images = []
+        self.image_counter = 0
+
+        # Bilder laden
+        image_path = './images'
+
+        # Bilder laden
+        for file in glob.glob(os.path.join(image_path, '*.jpg')) + glob.glob(os.path.join(image_path, '*.png')):
+            i = Image.open(file)
+            self.images.append(ImageTk.PhotoImage(resize(i, 400)))
+
+        #Grid aufbauen
+
+        self.image_label = Label(self.root, image=self.images[self.image_counter])
+        self.image_label.grid(row=0, column=0, columnspan=3)
+
+        back_button = Button(self.root, text='<<', command=lambda : self.skip(-1))
+        exit_button = Button(self.root, text='Bye', command=self.root.quit)
+        next_button = Button(self.root, text='>>', command=lambda : self.skip(+1))
+
+        back_button.grid(row=1, column=0)
+        exit_button.grid(row=1, column=1)
+        next_button.grid(row=1, column=2)
+
+        self.statusbar = Label(self.root, text='dummy', anchor=W, bd=1, relief=SUNKEN)
+        self.statusbar.grid(row=2, column=0, columnspan=3, sticky=E + W, )
+
+    def skip(self, number):
+        if 0 <= (self.image_counter+number) < len(self.images):
+            self.image_counter += number
+            self.image_label.grid_forget()
+            self.image_label = Label(self.root, image=self.images[self.image_counter])
+            self.image_label.grid(row=0, column=0, columnspan=3)
+
+            self.statusbar.grid_forget()
+            self.statusbar = Label(self.root, text=f'Image {self.image_counter} of {len(self.images)}', anchor=W, bd=1, relief=SUNKEN)
+            self.statusbar.grid(row=2, column=0, columnspan=3, sticky=E + W, )
+
+
+
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Image Viewer')
+    root.iconbitmap('folderpictures.ico')
+    root.geometry('400x350')
+
+    iv = ImageViewer(root)
+
+    root.mainloop()
